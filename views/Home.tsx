e01f0abaa3fc54b95e1407c5bf51184242bf6d25
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-11-02 13:08:47 +0500

    add the data in map function

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,13 +1,13 @@
 // "use client"
 import Hero from '@/components/LandingPage/Hero'
-import Project from '@/components/LandingPage/Project'
+import Project from '@/components/LandingPage/Project/Project'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Project/>
+      <Project/> 
     </>
   )
 }
